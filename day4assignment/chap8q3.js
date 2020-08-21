ac247280c8fdf60f3ce3cdc6da58b6e1f3d5e199
Hung Le <dangky1410@gmail.com>
2020-08-21 16:16:22 -0500

    Update chap8q3.js

--- day4assignment/chap8q3.js
+++ day4assignment/chap8q3.js
@@ -1,6 +1,6 @@
 let prompt = require('prompt-sync')();
 let n = prompt("Please enter an interger number: ");
 n = parseInt(n);
-for (let i = 1; i <=n ; i++) {
+for (let i = 2; i <n ; i++) {
     console.log(i);
 }

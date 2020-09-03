2b8b07561a8cc3679b61253978fd9499de805433
Hung Le <dangky1410@gmail.com>
2020-09-02 19:03:29 -0500

    Update question7.js

--- final_review/question7.js
+++ final_review/question7.js
@@ -1,24 +1,24 @@
-// function findSmallest(a, b, c) {
-// let arr = new Array(3);
-// arr[0] = a;
-// arr[1] = b,
-// arr[2] = c;
-// let new_arr = arr.sort((a, b) => a-b);
-// return new_arr[0];
-
-// }
-
-
 function findSmallest(a, b, c) {
-    let arr = new Array(3);
-    arr[0] = a;
-    arr[1] = b;
-    arr[2] = c;
-    return Math.min(...arr);
+let arr = new Array(3);
+arr[0] = a;
+arr[1] = b,
+arr[2] = c;
+let new_arr = arr.sort((a, b) => a-b);
+return new_arr[0];
+
 }
+
+
+// function findSmallest(a, b, c) {
+//     let arr = new Array(3);
+//     arr[0] = a;
+//     arr[1] = b;
+//     arr[2] = c;
+//     return Math.min(...arr);
+// }
 
 let a = 2;
 let b = -1;
-let c = 5;
+let c = -5;
 
 console.log(findSmallest(a, b, c));
